a4da472cc4fc33bd98de5ea8b379e7b144bc59a4
umn <langl269@umn.edu>
2025-11-10 15:13:49 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+  <section class="container">
+    <header>
+      <nav-bar/>
+    </header>
+
+    <section class="content">
+      <router-view></router-view>
+    </section>
+
+    <footer>Made by Nik</footer>
+  </section>
+</template>
+
+<style scoped>
+section.container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  > * {
+    border: 1px solid white;
+  }
+
+  .content {
+    background-color: white;
+    color: black;
+    padding: 10px;
+    border-radius: 10px;
+
+    p {
+      &:first-of-type {
+        margin-top: 0;
+      }
+
+      &:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  footer {
+    text-align: center;
+    padding: 10px;
+  }
+}
+</style>
